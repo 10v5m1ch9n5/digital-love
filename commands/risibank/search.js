72c62120a8bcf27bf30fb1db7f7e1f1d24dc4206
Lucas Loiseau <lucas.loiseau@insa-lyon.fr>
2022-02-27 01:57:02 +0100

    Support de la recherche avec plusiers mots clefs

--- commands/risibank/search.js
+++ commands/risibank/search.js
@@ -2,14 +2,21 @@ const { risibankAPI } = require('./../../axios-instances.js');
 
 module.exports = {
 	command: async (message, argv) => {
+		const helpMsg = `Mode d'emploi : \`--risibank search <mots-clefs>\``;
+		if(argv.length === 1) {
+			message.channel.send(helpMsg);
+			return;
+		}
+
 		const res = await risibankAPI.request({
 			url: '/medias/search',
 			params: {
-				query: argv[1],
+				query: argv.slice(1).join(' '),
 				category: 'sticker',
 				page: 1
 			}
 		});
+
 		const media = res.data.medias;
 		if(media.length === 0) {
 			message.channel.send('Aucun résultat :(');
