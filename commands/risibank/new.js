e3b2b44bca4f2e46c74fb0cbd37501445d855350
Lucas Loiseau <lucas.loiseau@insa-lyon.fr>
2022-02-27 02:45:28 +0100

    risitas new implémenté

--- commands/risibank/new.js
+++ commands/risibank/new.js
@@ -2,7 +2,29 @@ const { risibankAPI } = require('./../../axios-instances.js');
 
 module.exports = {
 	command: async (message, argv) => {
-		message.channel.send('Non implémenté :sleeping:');
-		//TODO: implémenter
+		const helpMsg = `Mode d'emploi \`--risibank new [help|nb]\`\nAffiche les \`nb\` derniers stickers postés sur risibank.\nPar défaut, \`nb=5\` et \`nb<=10\`.`;
+		if(argv[1] && Number.isNaN(parseInt(argv[1]))) {
+			message.channel.send(helpMsg);
+			return;
+		}
+
+		const res = await risibankAPI.request({
+			url: '/medias/new',
+			params: {
+				page: 1
+			}
+		});
+		const media = res.data;
+
+		const n = Math.min(10, parseInt(argv[1]) || 5);
+		const embedz = [];
+		for (let i = 0; i < n; i++) {
+			embedz[i] = {
+				thumbnail: {
+					url: media[i].source_url
+				}
+			}
+		}
+		message.channel.send({ embeds: embedz });
 	}
 }
